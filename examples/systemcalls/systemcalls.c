a8ba22a2178361f7f0d85f2dffab8222e5d7e80f
Matthew Harrison <Matthew.Harrison-1@colorado.edu>
2026-02-10 17:08:27 -0700

    First cut, not working

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,9 @@
 #include "systemcalls.h"
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -11,13 +16,12 @@ bool do_system(const char *cmd)
 {
 
 /*
- * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
 
-    return true;
+    return system(cmd) == 0;
 }
 
 /**
@@ -36,6 +40,12 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+    pid_t pid = 0;
+    pid_t wait_pid = 0;
+    int execv_return = 0;
+    int wstatus = 0;
+    bool return_val = false;
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -45,12 +55,8 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
 /*
- * TODO:
  *   Execute a system command by calling fork, execv(),
  *   and wait instead of system (see LSP page 161).
  *   Use the command[0] as the full path to the command to execute
@@ -58,19 +64,60 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    switch (pid = fork()) {
+    case -1:
+        // fork failed
+        return_val = false;
+        perror("fork");
+        break;
+    case 0:
+        // kid
+        execv_return = execv(command[0], &command[1]);
+        return_val = execv_return != -1;
+        if (!return_val) {
+            perror(command[0]);
+        }
+        break;
+    
+    default:
+        // parent
+        wait_pid = waitpid(pid, &wstatus, 0);
+
+        // use MACROS to make sure that the wait worked
+        return_val = WIFEXITED(wstatus);
+        if (!return_val) {
+            perror("Child did not exit succesfully");
+        }
+        else {
+            return_val &= wait_pid == pid;
+        }
+    }
 
     va_end(args);
 
-    return true;
+    return return_val;
 }
 
 /**
 * @param outputfile - The full path to the file to write with command output.
 *   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
+* @param ... - A list of 1 or more arguments after the @param count argument.
+*   The first is always the full path to the command to execute with execv()
+*   The remaining arguments are a list of arguments to pass to the command in execv()
+* @return true if the command @param ... with arguments @param arguments were executed successfully
+*   using the execv() call, false if an error occurred, either in invocation of the
+*   fork, waitpid, or execv() command, or if a non-zero return value was returned
+*   by the command issued in @param arguments with the specified arguments.
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    pid_t pid = 0;
+    pid_t wait_pid = 0;
+    int execv_return = 0;
+    int wstatus = 0;
+    int fd = 0;
+
+    bool return_val = false;
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -80,9 +127,6 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
 
 /*
@@ -92,8 +136,49 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) {
+        return_val = false;
+        perror("open");
+    }
+    else {
+        switch (pid = fork()) {
+        case -1:
+            // fork failed
+            return_val = false;
+            perror("fork");
+            break;
+        case 0:
+            // kid
+            if (dup2(fd, 1) < 0) {
+                return_val = false;
+            }
+            else {
+                execv_return = execv(command[0], &command[1]);
+                return_val = execv_return != -1;
+                if (!return_val) {
+                    perror(command[0]);
+                }
+            }
+            close(fd);
+            break;
+        
+        default:
+            // parent
+            wait_pid = waitpid(pid, &wstatus, 0);
+
+            // use MACROS to make sure that the wait worked
+            return_val = WIFEXITED(wstatus);
+            if (!return_val) {
+                perror("Child did not exit succesfully");
+            }
+            else {
+                return_val &= wait_pid == pid;
+            }
+        }
+    }
 
     va_end(args);
 
-    return true;
+    return return_val;
 }
